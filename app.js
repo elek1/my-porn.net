4c5daa3a98062e7474c2727d02df1857732fe8f2
elek1 <elekhose@gmail.com>
2021-02-22 22:44:06 +0100

    Hooked up the route

--- app.js
+++ app.js
@@ -3,5 +3,6 @@ const app = ex()
 
 app.use('/', require('./routes/index.js'))
 app.use('/comic', require('./routes/comic'))
+app.use('/about', require('./routes/about'))
 
 app.listen(3000)
---
+++ routes/about.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const router = express.Router()
+const pug = require('pug')
+
+router.get('/', (req, res) => {
+    res.send(pug.renderFile('views/about.pug'))
+})
+
+module.exports = router
