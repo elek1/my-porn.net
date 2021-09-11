be5cb8a9d353af21261af0d67342ea272127cd13
elek1 <elekhose@gmail.com>
2021-09-11 20:24:31 +0200

    created getComics

--- database.js
+++ database.js
@@ -23,3 +23,39 @@ exports.getComic = (id) => {
         })
     })
 }
+
+exports.getComics = (count, page, filters) => {
+    return new Promise((resolve, reject) => {
+        const query = {}
+        if (filters.hasOwnProperty('search')) {
+            query.$or = []
+            filters.search.forEach((term) => {
+                query.$or.push({ tags: { $regex: `.*${term}.*`, $options: 'i' } })
+                query.$or.push({ author: { $regex: `.*${term}.*`, $options: 'i' } })
+                query.$or.push({ title: { $regex: `.*${term}.*`, $options: 'i' } })
+            })
+        }
+        switch (filters.sort) {
+            case 'tasc':
+                filters.sort = { title: 1 }
+                break
+            case 'tdesc':
+                filters.sort = { title: -1 }
+                break
+            default:
+                filters.sort = { title: 1 }
+                break
+        }
+        dbConnection.then((db) => {
+            db.db('YiffCollections')
+                .collection('comic')
+                .find(query)
+                .sort(filters.sort)
+                .limit(page * count)
+                .toArray((err, result) => {
+                    if (err) throw err
+                    resolve(result.splice((page - 1) * count, page * count))
+                })
+        })
+    })
+}
