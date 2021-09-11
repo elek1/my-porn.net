708c427dca9751d9ba4eb2ace711094c000afd26
elek1 <elekhose@gmail.com>
2021-09-11 19:23:39 +0200

    moved data to database.js

--- routes/comic.js
+++ routes/comic.js
@@ -1,37 +1,12 @@
 const express = require('express')
 const router = express.Router()
 const path = require('path')
-// const db = require('../database')
+const db = require('../database')
 const fs = require('fs')
 const pug = require('pug')
 
-router.get('/', (req, res) => {
-    res.send(pug.renderFile('views/comic.pug', {
-        title: 'test',
-        authors: [
-            'hallo',
-            'y o u'
-        ],
-        tags: [
-            'test',
-            'yes'
-        ],
-        status: 'completed',
-        pages: [
-            {
-                'low-quality': 'https://static1.e621.net/data/sample/eb/d3/ebd39570b1917e10e1f6a85190215294.jpg',
-                'high-quality': 'https://static1.e621.net/data/eb/d3/ebd39570b1917e10e1f6a85190215294.png'
-            },
-            {
-                'low-quality': 'https://static1.e621.net/data/sample/3e/57/3e57ac46fd4d1c916d47e8ce357918f2.jpg',
-                'high-quality': 'https://static1.e621.net/data/3e/57/3e57ac46fd4d1c916d47e8ce357918f2.png'
-            },
-            {
-                'low-quality': 'https://static1.e621.net/data/sample/58/59/5859473a1e760366fc0a72d1f69bbae1.jpg',
-                'high-quality': 'https://static1.e621.net/data/58/59/5859473a1e760366fc0a72d1f69bbae1.png'
-            }
-        ]
-    }))
+router.get('/', async (req, res) => {
+    res.send(pug.renderFile('views/comic.pug', await db.getComic()))
 })
 
 /*router.get('/', (req, res) => {
