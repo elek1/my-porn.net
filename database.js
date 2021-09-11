bd5c0ef4ba178840359e911ed9d4c00f3d3bb8f0
elek1 <elekhose@gmail.com>
2021-09-11 19:43:31 +0200

    connected mongodb

--- database.js
+++ database.js
@@ -8,31 +8,18 @@ var dbConnection = client.connect()
 
 exports.getComic = (id) => {
     return new Promise((resolve, reject) => {
-        resolve({
-            title: 'test',
-            authors: [
-                'hallo',
-                'y o u'
-            ],
-            tags: [
-                'test',
-                'yes'
-            ],
-            status: 'completed',
-            pages: [
-                {
-                    'low-quality': 'https://static1.e621.net/data/sample/eb/d3/ebd39570b1917e10e1f6a85190215294.jpg',
-                    'high-quality': 'https://static1.e621.net/data/eb/d3/ebd39570b1917e10e1f6a85190215294.png'
-                },
-                {
-                    'low-quality': 'https://static1.e621.net/data/sample/3e/57/3e57ac46fd4d1c916d47e8ce357918f2.jpg',
-                    'high-quality': 'https://static1.e621.net/data/3e/57/3e57ac46fd4d1c916d47e8ce357918f2.png'
-                },
-                {
-                    'low-quality': 'https://static1.e621.net/data/sample/58/59/5859473a1e760366fc0a72d1f69bbae1.jpg',
-                    'high-quality': 'https://static1.e621.net/data/58/59/5859473a1e760366fc0a72d1f69bbae1.png'
-                }
-            ]
+        dbConnection.then((db) => {
+            db.db('YiffCollections')
+                .collection('comic')
+                .findOne({ id: id }, async (err, result) => {
+                    if (result === null) result = {}
+                    result.status = result.ongoing ? 'Ongoing' : 'Completed'
+                    result.authors = result.author
+
+                    result.pages = await result.pages.map(page => {return { lowquality: page.lq, highquality: page.hq}})
+
+                    resolve(result)
+                })
         })
     })
 }
