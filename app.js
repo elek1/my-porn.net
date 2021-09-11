27a53da7bcd909dbfb13199f7dcbd2eba4c8027a
elek1 <elekhose@gmail.com>
2021-09-11 20:15:29 +0200

    added list route

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const ex = require('express')
 const app = ex()
 
 app.use('/', require('./routes/index.js'))
+app.use('/list', require('./routes/list'))
 app.use('/comic', require('./routes/comic'))
 app.use('/about', require('./routes/about'))
 
---
+++ routes/list.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const router = express.Router()
+const db = require('../database')
+const pug = require('pug')
+
+router.get('/', async (req, res) => {
+    // Getting URL parameters
+    const page = isNaN(req.query.page) ? 1 : +req.query.page
+    const count = isNaN(req.query.count)
+        ? 12
+        : +req.query.count <= 0 || +req.query.count > 50
+        ? 12
+        : +req.query.count
+    const sort = typeof req.query.sort == 'undefined' ? 'tasc' : req.query.sort
+    const search = typeof req.query.search == 'undefined' ? '' : req.query.search
+
+    const filters = { sort: sort }
+
+    // window.history.pushState({}, "", "list.html")
+
+    if (search.length > 0){
+        filters.search = []
+        search.split(' ').forEach((val) => filters.search.push(val.replace('_', ' ')))
+    }
+    res.send(pug.renderFile('views/list.pug'))
+
+    //res.send(await db.getComics(count, page, filters))
+})
+
+module.exports = router
