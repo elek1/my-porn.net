0cdadfbe1dfcc8104302215ac4c5147e94780b41
elek1 <elekhose@gmail.com>
2021-09-12 15:41:35 +0200

    serverside implementation of filters

--- routes/list.js
+++ routes/list.js
@@ -5,25 +5,30 @@ const pug = require('pug')
 
 router.get('/', async (req, res) => {
     // Getting URL parameters
-    const page = isNaN(req.query.page) ? 1 : +req.query.page
+    const page = isNaN(req.query.page) ? 1 : +req.query.page // TODO page
+    const title = typeof req.query.title == 'undefined' ? '' : req.query.title
+    const tags = typeof req.query.tags == 'undefined' ? [] : req.query.tags.split(' ')
+    const author = typeof req.query.author == 'undefined' ? '' : req.query.author
+    const sort = typeof req.query.sort == 'undefined' ? 'tasc' : req.query.sort
     const count = isNaN(req.query.count)
         ? 12
         : +req.query.count <= 0 || +req.query.count > 50
         ? 12
         : +req.query.count
-    const sort = typeof req.query.sort == 'undefined' ? 'tasc' : req.query.sort
-    const search = typeof req.query.search == 'undefined' ? '' : req.query.search
-
-    const filters = { sort: sort }
-
-    // window.history.pushState({}, "", "list.html")
-
-    if (search.length > 0){
-        filters.search = []
-        search.split(' ').forEach((val) => filters.search.push(val.replace('_', ' ')))
-    }
+    const allowOngoing = typeof req.query.allowOngoing == 'undefined' ? true : (req.query.allowOngoing == 'true' ? true : false)
     
-    res.send(pug.renderFile('views/list.pug', { search: search, comics: await db.getComics(count, page, filters) }))
+    res.send(pug.renderFile('views/list.pug', {
+        possibleAuthors: await db.getAuthors(),
+        search: {
+            title: title,
+            tags: tags,
+            author: author,
+            sort: sort,
+            count: count,
+            allowOngoing: allowOngoing
+        },
+        comics: await db.getComics(title, tags, author, sort, count, allowOngoing)//count, page, filters) 
+    }))
 })
 
 module.exports = router
