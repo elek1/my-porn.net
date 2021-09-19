25030e49370376499030adbd8ba764c65dc551cf
elek1 <elekhose@gmail.com>
2021-09-19 11:41:37 +0200

    added TODO

--- routes/import.js
+++ routes/import.js
@@ -58,7 +58,7 @@ function getInfo(url) {
 
                     returnJSON.source = 'yiffer.xyz'
                     returnJSON.title = json.querySelector('.loadedComicHeader').innerText.trim()
-                    returnJSON.author.push(json.querySelector('.artistNameLink').innerText.toLowerCase().trim())
+                    returnJSON.author.push(json.querySelector('.artistNameLink').innerText.toLowerCase().trim())  // TODO change to authors
                     //returnJSON.cover = 'https://static.yiffer.xyz/comics' + url.substr(url.indexOf('/', 9)) + (url.endsWith('/') ? '' : '/') + 'thumbnail.jpg'
                     returnJSON.cover = returnJSON.pages[0].lq
 
