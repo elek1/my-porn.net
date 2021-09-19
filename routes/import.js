a57e4a568744b7a2985ed35f387f251f7ad86f45
elek1 <elekhose@gmail.com>
2021-09-19 10:00:33 +0200

    created getInfo function

--- routes/import.js
+++ routes/import.js
@@ -11,4 +11,36 @@ router.get('/', async (req, res) => {
 
 router.post('/', async (req, res) => {
 })
+
+function getInfo(url) {
+    return new Promise(async(resolve, reject) => {
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
+        await page.goto(url);
+        setTimeout(async () => {
+            const json = HTMLParser.parse(await page.content())
+
+            const returnJSON = {
+                source: '',
+                'source link': url,
+                author: [],
+                cover: '',
+                title: '',
+                tags: [],
+                pages: []
+            }
+
+            switch (url.substr(0, url.indexOf('/', 9))) {
+                default:
+                    reject()
+                    return
+            }
+
+            resolve(returnJSON)
+
+            await browser.close();
+        }, 1000)
+    })
+}
+
 module.exports = router
